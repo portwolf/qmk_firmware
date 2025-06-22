1b8bfa197d53b841a0317cd4e0e0ee7049aa5051
portwolf <portwolf@none.com>
2025-06-22 09:50:31 +0200

    add SPLIT_WATCHDOG_ENABLE to redox/keymap/portwolf/config.h

--- keyboards/redox/keymaps/portwolf/config.h
+++ keyboards/redox/keymaps/portwolf/config.h
@@ -1,20 +1,3 @@
-/*
-Copyright 2018 Mattia Dal Ben <matthewdibi@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 // required for correct detection of keyboard
@@ -23,6 +6,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_USB_TIMEOUT 3000
 // define poll frequency for split_usb_detect
 #define SPLIT_USB_TIMEOUT_POLL 10
+// enable software watchdog on slave side, reboots keyb if no successful coms within SPLIT_USB_TIMEOUT
+#define SPLIT_WATCHDOG_ENABLE
 
 // Use i2c for comms between halfs
 #define USE_I2C
